6d9a72e94fa7f1a383442e6280c0d9f5d5bb55df
Mehbark <clarkemailtest@gmail.com>
2021-07-22 15:50:18 -0400

    trying to get imports/exports to work

---
+++ repeat.mjs
@@ -0,0 +1,35 @@
+/**
+ * Repeat the function `func` `times` times, passing `args` as the arguments to it every time.
+ * @param {function} func The function you want to run each time.
+ * @param {number} times A **positive** number for how many times you want to run the function. **If it is a floating point, it will be rounded down.**
+ * @param {...*=} args Arguments to be passed to `func`.
+ */
+export function repeat(func, times, ...args) {
+  //Check that times is a usable int or float
+  if (!Number.isFinite(times)) {
+    throw new Error(
+      "Something that isn't a number was passed to times, or nothing at all."
+    );
+  } else if (times < 0) {
+    throw new Error(
+      "A number less than 0 was passed to times, that won't work."
+    );
+  }
+  times = Math.floor(times); //At this point we now know we are working with an integer
+
+  for (let i = 0; i < times; i++) {
+    func(...args); 
+  }
+}
+
+/**
+ * Repeat the function `func` `times` times, passing `args` as the arguments to it every time.
+ * @param {function} func The function you want to run each time.
+ * @param {number} times A **positive** number for how many times you want to run the function. **If it is a floating point, it will be rounded down.**
+ * @param {...*=} args Arguments to be passed to `func`.
+ */
+export function t(func, times, ...args) {
+  repeat(func, times, ...args);
+}
+
+export { repeat, t };
--- tests.js
+++ tests.js
@@ -1,5 +1,14 @@
+const repea = require("https://mehbark.github.io/repeatjs/repeat.mjs");
+
 function hello(greeting, punctuation) {
   console.log(greeting + punctuation);
 }
 
-t(hello, 2, "hi", "!");
+repea.t(hello, 2, "hi", "!");
+repea.t(hello, 2.1, "hi", "!");
+repea.t(hello, 0, "hi", "!");
+repea.t(hello, 2);
+repea.t(hello, -1, "hi", "!").catch(err => console.log(err));
+repea.t(hello, -1, "hi", "!").catch(err => console.log(err));
+repea.t(hello, "ten", "hi", "!".catch(err => console.log(err)));
+repea.t(hello).catch(err => console.log(err));
